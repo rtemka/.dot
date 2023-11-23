e09a104877cf38f37c9f5566db1a4d40c0568002
rtemka <rybakov333@gmail.com>
2023-11-23 16:18:56 +0300

    commented bashls autocommand

--- config/.config/nvim/lua/config/autocommands.lua
+++ config/.config/nvim/lua/config/autocommands.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+--[[ Bash language server ]]
+--[[ vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'sh',
+  callback = function()
+    vim.lsp.start({
+      name = 'bash-language-server',
+      cmd = { 'bash-language-server', 'start' },
+    })
+  end,
+}) ]]
