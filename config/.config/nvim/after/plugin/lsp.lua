5116d2a8ab633b28e69181a61669ad677fd45cfc
Артем Рыбаков <artem.rybakov@vkteam.ru>
2025-08-07 12:25:22 +0300

    rm double go_pls config

--- config/.config/nvim/after/plugin/lsp.lua
+++ config/.config/nvim/after/plugin/lsp.lua
@@ -7,14 +7,14 @@ vim.filetype.add {
         j2 = 'jinja',
     },
 }
-local lspconfig = require("lspconfig")
-lspconfig.gopls.setup({
-    settings = {
-        gopls = {
-            gofumpt = true, -- Enable gofumpt for formatting
-        },
-    },
-})
+-- local lspconfig = require("lspconfig")
+-- lspconfig.gopls.setup({
+--     settings = {
+--         gopls = {
+--             gofumpt = true, -- Enable gofumpt for formatting
+--         },
+--     },
+-- })
 -- --  This function gets run when an LSP connects to a particular buffer.
 -- local on_attach = function(_, bufnr)
 --     -- In this case, we create a function that lets us more easily define mappings specific
